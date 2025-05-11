2ce2e07a8bbf67c1dcd2cb637e8bbee80f0bff33
yadu <kyadushree47@gmail.com>
2025-05-11 17:19:15 +0530

    home page issues resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import Login from "@/pages/Login";
 import Signup from "@/pages/Signup";
-import Home from "../pages/Home"
+import Home from "@/pages/Home"
 import PrivateRoute from "@/components/PrivateRoute"; 
 
 const App = () => {
